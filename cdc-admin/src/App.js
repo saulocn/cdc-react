74aeb37a9f065d2e7bfa91bdabd9bf7b0c42c212
Saulo <saulocn@gmail.com>
2016-10-18 21:04:13 -0200

    - Início de estudo sobre React com create-react-app;

---
+++ cdc-admin/src/App.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import './css/pure-min.css';
+import './css/side-menu.css';
+import $ from 'jquery';
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      lista:[/*   {
+        nome:'Saulo',
+        email:'saulocn@gmail.com',
+        senha:'123456',
+      }*/],
+      nome:'',
+      email:'',
+      senha:''
+    };
+
+    this.enviaForm=this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+
+
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }
+
+  setSenha(evento){
+    this.setState({Senha:evento.target.value});
+  }
+
+componentDidMount(){
+  console.log("DidMount")
+}
+
+// para subir a aplicação, deve se executar o comando
+// java  -Dspring.datasource.password=<senha do root do mysql> -jar cdcreact-1.0.0.jar
+componentWillMount(){
+  console.log("componentWillMount");
+  $.ajax({
+    url: 'http://localhost:8080/api/autores',
+    dataType: 'json',
+    success:function(res){
+        this.setState({lista:res});
+    }.bind(this)
+  })
+}
+
+// sintatic event do React e não evento do DOM
+enviaForm(evento){
+  evento.preventDefault();
+  console.log("dados sendo enviados");
+  $.ajax({
+    url:'http://localhost:8080/api/autores',
+    contentType:'application/json',
+    dataType:'json',
+    type:'post',
+    data:JSON.stringify({nome:this.state.nome,email:this.state.email, senha:this.state.senha}),
+    success:function(resposta){
+      console.log("Enviado com sucesso!");
+    },
+    error:function(resposta){
+      console.log("Deu erro");
+    }
+  });
+}
+
+
+  render() {
+    return (
+     <div id="layout">
+    {/*<!-- Menu toggle -->*/}
+    <a href="#menu" id="menuLink" className="menu-link">
+       {/* <!-- Hamburger icon -->*/}
+        <span></span>
+    </a>
+
+    <div id="menu">
+        <div className="pure-menu">
+            <a className="pure-menu-heading" href="#">Company</a>
+
+            <ul className="pure-menu-list">
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livros</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="main">
+            <div className="header">
+              <h1>Cadastro de Autores</h1>
+            </div>
+            <div className="content" id="content">
+              <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                  <div className="pure-control-group">
+                    <label htmlFor="nome">Nome</label> 
+                    <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />                  
+                  </div>
+                  <div className="pure-control-group">
+                    <label htmlFor="email">Email</label> 
+                    <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />                  
+                  </div>
+                  <div className="pure-control-group">
+                    <label htmlFor="senha">Senha</label> 
+                    <input id="senha" type="password" name="senha"  value={this.state.senha} onChange={this.setSenha} />                                      
+                  </div>
+                  <div className="pure-control-group">                                  
+                    <label></label> 
+                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                  </div>
+                </form>             
+
+              </div>  
+              <div>            
+                <table className="pure-table">
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>email</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    { 
+                      this.state.lista.map(function(autor){
+                        return (
+                          <tr key={autor.id}>
+                            <td>{autor.nome}</td>
+                            <td>{autor.email}</td>
+                          </tr>
+                          );
+                      })
+                    }
+                  </tbody>
+                </table> 
+              </div>             
+            </div>
+          </div>         
+</div>
+    );
+  }
+}
+
+export default App;
